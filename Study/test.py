46ee0662d634bc089d333ad2cb57f2e84d23c664
minsangggg <parkss6468@gmail.com>
2025-08-29 09:59:19 +0900

    원격DB가져오기

--- Study/test.py
+++ Study/test.py
@@ -5,7 +5,7 @@ try:
     conn_src = mariadb.connect(
         user="lguplus7",
         password="lg7p@ssw0rd~!",
-        host="192.168.14.40",
+        host="192.168.14.98",
         port=3310,
         database="bangu"
     )
@@ -23,25 +23,34 @@ except mariadb.Error as e:
 src_cur = conn_src.cursor()
 tar_cur = conn_tar.cursor()
 
-src_cur.execute("select * from room")
+# -------------------
+# ROOM 테이블
+# -------------------
+src_cur.execute("SELECT * FROM room")
 res = src_cur.fetchall()
-print(f'fetched {len(res)} records')
-tar_cur.execute('delete from room')
-conn_tar.commit()
+print(f'fetched {len(res)} records from source.room')
+
 for record in res:
-    tar_cur.execute("insert into room values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" , (record))
-    conn_tar.commit()
-    
-    
-src_cur.execute("select * from images")
-res2 = src_cur.fetchall()
-print(f'fetched {len(res2)} records')
-tar_cur.execute('delete from images')
+    # room 테이블이 27개의 컬럼이라고 가정
+    tar_cur.execute("""
+        INSERT IGNORE INTO room VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, record)
 conn_tar.commit()
+
+# -------------------
+# IMAGES 테이블
+# -------------------
+src_cur.execute("SELECT * FROM images")
+res2 = src_cur.fetchall()
+print(f'fetched {len(res2)} records from source.images')
+
 for record in res2:
-    tar_cur.execute("insert into images values (?,?,?,?,?,?)" , (record))
-    conn_tar.commit()
-        
+    tar_cur.execute("""
+        INSERT IGNORE INTO images VALUES (?, ?, ?, ?, ?, ?)
+    """, record)
+conn_tar.commit()
+
+# 연결 종료
 src_cur.close()
 conn_src.close()
 tar_cur.close()
